271e144f9d163641938f9f755ae4c9b8b4543714
BARANEESWARAN <baranees1@gmail.com>
2024-08-13 14:15:57 +0530

    changing counter

--- src/components/Context/ContextParent.js
+++ src/components/Context/ContextParent.js
@@ -1,14 +1,12 @@
 import React, { useState } from "react";
 import { ChildA } from "./ContextChildren";
 const ContextParent = () => {
-  const [count, setCount] = useState();
+  const [count, setCount] = useState(0);
   console.log("parentContext render");
   return (
     <>
       <div>ContextParent</div>
-      <button onClick={() => setCount((prev) => prev + 1)}>
-        count-{count}
-      </button>
+      <button onClick={() => setCount((c) => c + 1)}>count-{count}</button>
       <ChildA />
     </>
   );
